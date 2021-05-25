9e4badb04c5a7e7a0594b8cfed8c968e08f5e313
krishnamnuwal <67596324+krishnamnuwal@users.noreply.github.com>
2021-05-26 03:04:59 +0530

    Add files via upload

--- jarvis.py
+++ jarvis.py
@@ -5,16 +5,30 @@ import datetime
 import speech_recognition as sr
 import wikipedia
 import webbrowser
+import os
+import random
+import smtplib
+import subprocess
+
 engine=pyttsx3.init('sapi5')
 
 # print(engine)
 voices=engine.getProperty('voices')
 # print(voices[0].id)
 
-engine.setProperty('voice',voices[1].id)
+engine.setProperty('voice',voices[0].id)
 
-
-
+###To use email function there is need to turn on the less secure app access 'on'
+###otherwise email will not be sent 
+###https://support.google.com/accounts/answer/6010255?hl=en#zippy=%2Cif-less-secure-app-access-is-off-for-your-account%2Cif-less-secure-app-access-is-on-for-your-account
+        ##Through this link you can turn it on 
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('write your email id', 'write your email id password')
+    server.sendmail('write your email id', to, content)
+    server.close()
 
 def speak(audio):
   engine.say(audio)
@@ -34,7 +48,7 @@ def wishMe():
       # speak(hour)
       # speak('pm')
 
-  speak("I am Jarvis .Please tell me how may I help you")
+  speak("I am Jarvis your virtual artificial intelligence! Please tell me how could I help you")
 
 def takeCommand():
   #It Takes microphone input from users and returns string output
@@ -62,6 +76,8 @@ if __name__ == "__main__":
     while True:
       query=takeCommand().lower()
 
+      
+
       if 'wikipedia' in query:
         speak('searching wikipedia...')
         query=query.replace("wikipedia","")
@@ -74,6 +90,93 @@ if __name__ == "__main__":
 
       elif 'open youtube' in query:
         speak('opening youtube')
-        c=webbrowser.get('chrome')
-        c.open("youtube.com")
-    
+        webbrowser.register('chrome',None)
+        webbrowser.open('https://www.youtube.com')
+
+      elif 'open google' in query:
+        speak('opening google')
+        webbrowser.open('https://www.google.com')
+
+      elif 'open github' in query:
+        speak('opening github')
+        webbrowser.open('https://www.github.com')
+
+      elif 'open white hat' in query:
+        speak('opening whitehatjr')
+        webbrowser.open('https://code.whitehatjr.com')
+
+      elif 'play music' in query:
+        speak('playing music')
+        music_dir='E:\music'
+        songs=os.listdir(music_dir)
+        print(songs)
+        os.startfile(os.path.join(music_dir,random.choice(songs)))
+
+      elif 'the time' in query:
+        strTime=datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"Sir,the time is {strTime}")
+
+      elif 'open visual studio code' in query:
+        speak('opening visual studio code')
+        codePath = "C:\\Users\\shree\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+        os.startfile(codePath)
+
+      elif 'open sublime' in query:
+        speak('opening sublime')
+        limePath="C:\\Program Files\\Sublime Text 3\\sublime_text.exe"
+        os.startfile(limePath)
+
+      elif 'open telegram' in query:
+        speak('opening telegram')
+        telPath="C:\\Users\\shree\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe"
+        os.startfile(telPath)
+
+      elif 'open brave' in query:
+        speak('opening brave')
+        brPath = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+        os.startfile(brPath)
+      
+      elif 'open chrome' in query:
+        speak('opening chrome')
+        crPath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+        os.startfile(crPath)
+      
+      elif 'open d' in query:
+        speak('opening drive d')
+        dPath='D://'
+        os.startfile(dPath)
+      
+      elif 'open e' in query:
+        speak('opening drive E')
+        ePath = 'E://'
+        os.startfile(ePath)
+
+      elif 'open c drive' in query:
+        speak('opening drive C')
+        cPath = 'C://'
+        os.startfile(cPath)
+
+      elif 'send email to krishnam' in query:
+         try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "write the email address to whom you want to send the mail"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+         except Exception as e:
+                print(e)
+                speak("Sorry Sir,I'm unable to send email")  
+      
+   
+      
+      elif 'search' in query:
+        query=query.replace("search","")
+        webbrowser.register('chrome', None)
+        speak(f"searching {query}")
+        webbrowser.open(query)
+      
+      elif "log off" in query or "sign out" in query or "shut down" in query:
+          speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+          subprocess.call(["shutdown", "/l"])
+      elif 'quit' in query or 'stop' in query or 'bye' in query:
+        exit()
